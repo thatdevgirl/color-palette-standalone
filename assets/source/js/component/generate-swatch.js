462adbdd8f7de988088d922ee3ea47de191ec279
Joni Halabi <her@thatdevgirl.com>
2022-09-17 21:03:45 -0400

    initial commit

--- assets/source/js/component/generate-swatch.js
+++ assets/source/js/component/generate-swatch.js
@@ -7,7 +7,15 @@
 const GenerateSwatch = ( () => {
   $( document ).ready( () => {
 
-    console.log('hi there');
+    $( '#generate-swatch' ).submit( (e) => {
+      e.preventDefault();
+
+      const color = $( '#color' ).val();
+      
+      $( '.color-swatch' ).css( 'backgroundColor', color );
+      $( '.color-name' ).text( 'Color' );
+      $( '.color-hex').text( color );
+    } );
 
   } );
 })();
